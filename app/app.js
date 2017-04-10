4b26f38abf36615ce0b54021f8a3eb868d9a7387
Thomson <779387850@qq.com>
2017-04-10 22:34:22 +0800

    添加删除事项，功能雏形

---
+++ app/app.js
@@ -0,0 +1,43 @@
+import bar from './bar';
+import Vue from 'vue'
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        newTodo: '',
+        todoList: []
+    },
+    created: function() {
+        // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
+        window.onbeforeunload = () => {
+            let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
+            window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+        }
+
+        let oldDataString = window.localStorage.getItem('myTodos')
+        let oldData = JSON.parse(oldDataString)
+        this.todoList = oldData || []
+    },
+    methods: {
+        addTodo: function() {
+            this.todoList.push({
+                title: this.newTodo,
+                createdAt: new Date(),
+                done: false // 添加一个 done 属性
+            })
+
+            this.newTodo = '' // 变成空
+        },
+        removeTodo: function(todo) {
+            // 找到给定元素的第一个索引
+            let index = this.todoList.indexOf(todo)
+
+            // 从index开始删除1个元素
+            this.todoList.splice(index, 1)
+        }
+
+    }
+})
+
+
+bar();
